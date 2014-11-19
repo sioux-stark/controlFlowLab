36bf06a9c4dcc2450dd1c0f64a70d5398246d1f3
sandra stark <stark.sandy@gmail.com>
2014-11-18 19:28:33 -0800

    various loops

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,15 @@
 var data = require("./products.json")
 
-// Write your solutions below
 
-console.log(data["items"]);
+// console.log(data["items"]);
+console.log(data["items"].length)
+
+for (var i = 0; i < data["items"].length; i++) {
+	var object = data["items"][i]["product"]["inventories"];
+	console.log(object)
+};
+
+
+// console.log(data["items"][0]);
+// console.log(data["items"][1]);
+// console.log(data["items"][2]);
